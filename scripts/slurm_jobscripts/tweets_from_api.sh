8b4585c7cf040cdc37c3934f20e728e7db0d3873
Patrick Wall <patgwall@gl-login1.arc-ts.umich.edu>
2023-05-23 02:22:40 -0400

    hpc fixes

--- scripts/slurm_jobscripts/tweets_from_api.sh
+++ scripts/slurm_jobscripts/tweets_from_api.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=orgs
-#SBATCH --account=patgwall
-#SBATCH --partition=standard
+#SBATCH --job-name=download_journalists
 #SBATCH --time=08-00:00:00
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=5gb
+#SBATCH --output=log.out
 #SBATCH --mail-type=BEGIN,END,FAIL
 
 
