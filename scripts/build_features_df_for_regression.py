d6b6058fef5bbc1e99f83410e8ccd5737dc2d011
pat wall <godzilla-but-nicer@protonmail.com>
2023-12-08 10:07:40 -0500

    featuresgit add .

--- scripts/build_features_df_for_regression.py
+++ scripts/build_features_df_for_regression.py
@@ -107,7 +107,7 @@ for i, tweet in tqdm(filtered_tweets.iterrows()):
 
 
 features_df = pd.merge(meta_subset, pd.DataFrame(feature_rows), how="left", on="id_str")
-features_df.to_csv(paths["regressions"]["features"], sep="\t", index=False)
+features_df.to_csv(paths["regression"]["features"], sep="\t", index=False)
 
 with open(paths["mentions"]["neighbors"], "w") as neighbors_out:
     json.dump(mention_neighbors, neighbors_out)
